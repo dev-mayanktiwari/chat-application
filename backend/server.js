2af58de21a993390795c02c85774beba84013966
Mayank <mayanktiwari1212.mt@gmail.com>
2024-07-02 15:42:16 +0530

    build complete

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 // MODULE IMPORTS
+import path from "path";
 import express from "express";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
@@ -12,6 +13,8 @@ import { app, server } from "./socket/socket.js";
 // DATABASE CONNECT
 import connecttoMongoDB from "./db/connecttoMongoDB.js";
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
@@ -22,6 +25,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messagesRouter);
 app.use("/api/users", userRouter);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connecttoMongoDB();
